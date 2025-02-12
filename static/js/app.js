6cd191f40fae3b2fe87719f73c7ed32f84b1821e
UninspiredNomenclature <adwood104@gmail.com>
2025-02-12 07:33:51 -0700

    Pushing the complete files. Starter files committed in error previously.

--- static/js/app.js
+++ static/js/app.js
@@ -28,29 +28,65 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-// parallel to above
+    let samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
-
+    let sampleArray = samples.filter(sampleObject => sampleObject.id == sample);
+    let sampleResult = sampleArray[0];
 
     // Get the otu_ids, otu_labels, and sample_values
-
+    let otu_ids = sampleResult.otu_ids;
+    let otu_labels = sampleResult.otu_labels;
+    let sample_values = sampleResult.sample_values;
 
     // Build a Bubble Chart
+    let bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "Earth"
+        }
+      }
+    ];
+
+    let bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      margin: { t: 30 },
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" }
+    };
 
 
     // Render the Bubble Chart
-
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
+    let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
+    let barData = [
+      {
+        y: yticks,
+        x: sample_values.slice(0, 10).reverse(),
+        text: otu_labels.slice(0, 10).reverse(),
+        type: "bar",
+        orientation: "h"
+      }
+    ];
 
+    let barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      margin: { t: 30, l: 150 }
+    };
 
     // Render the Bar Chart
-
+    Plotly.newPlot("bar", barData, barLayout);
   });
 }
 
